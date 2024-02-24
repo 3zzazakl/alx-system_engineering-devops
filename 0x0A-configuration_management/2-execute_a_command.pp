baa7068c633dd02745da1ad96c644eda8a8b8594
3zzazakl <vanzekovichgr@gmail.com>
2024-02-24 02:45:26 +0200

    checking task2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow
 # and ensures that it is not running.
 exec { 'kill_process':
-  command     => '/usr/bin/pkill -f killmenow',
+  command     => '/usr/bin/pkill killmenow',
   refreshonly => true,
 }
