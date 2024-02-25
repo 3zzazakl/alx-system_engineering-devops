ddcb780099651597cfe7339c2f22d09bbd7480b7
3zzazakl <vanzekovichgr@gmail.com>
2024-02-25 08:51:36 +0200

    reviewing puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,1 @@
-file { 'pass':
-    ensure  => persent,
-    path    => '/etc/ssh/sshd_config',
-    line    => 'PasswordAuthentication no',
-}
-
-file { 'Key':
-    ensure  => present,
-    path    => '/etc/ssh/sshd_config',
-    line    => 'IdentityFile ~/.ssh/school',
-}
+# using puppet to configure the system
