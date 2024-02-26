be4cc2468c76e63d9bb731c90e8849c3ac1c9786
3zzazakl <vanzekovichgr@gmail.com>
2024-02-26 04:44:55 +0200

    checking task100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # using puppet to configure the system
 include stdlib
 
-file_line { '/etc/ssh/sshd_config':
+file_line {
+  '/etc/ssh/sshd_config':
   ensure => present,
   path   => '/etc/ssh/sshd_config',
   line   => 'PasswordAuthentication no',
-}
-file_line { '~/.ssh/school':
+  ;
+  '~/.ssh/school':
   ensure => present,
   path   => '/etc/ssh/sshd_config',
   line   => 'IdentityFile ~/.ssh/school',
