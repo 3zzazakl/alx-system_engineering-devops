f6db443013f7c053e827ea01c27335749dae78d0
3zzazakl <vanzekovichgr@gmail.com>
2024-02-26 03:09:25 +0200

    checking task100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,13 @@
 # using puppet to configure the system
+
+file { '/etc/ssh/sshd_config':
+  ensure  => present,
+  path    => '/etc/ssh/sshd_config',
+  content => 'PasswordAuthentication no',
+}
+
+file { '~/.ssh/scool':
+  ensure  => present,
+  path    => '~/.ssh/school',
+  content => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZt'
+}
