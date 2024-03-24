55be67fb4dca9b374a2e61f1efac96b422638c93
3zzazakl <vanzekovichgr@gmail.com>
2024-03-24 08:46:15 +0200

    checking puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,8 +12,7 @@ exec { 'root':
 exec { 'redirect':
   provider => shell,
   command  =>
-    'sudo sed -i "s/listen 80 default_server; listen 80 default_server\\n\\tlocation \/redirect_me \
-    {\\n\\t\\treturn 301 http:\/\/www.google.com;\\n\\t}\\n/g" /etc/nginx/sites-available/default',
+    'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \\/redirect_me {\\n\\t\\treturn 301 http:\/\/www.google.com\\/;\\n\\t}/" /etc/nginx/sites-available/default',
 }
 
 exec { 'restart':
