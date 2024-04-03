338c8c3004ad0f1096bbad1ef295af5d5c5d42ba
3zzazakl <vanzekovichgr@gmail.com>
2024-04-03 03:14:47 +0200

    checking puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+exec { 'update':
+  command  => 'sudo apt update',
+  provider => shell,
+}
+
+package { 'nginx':
+  ensure   => installed,
+}
+
+file_line { 'header':
+  ensure => present,
+  path   => '/etc/nginx/site-available/default',
+  line   => '  add-header X-Servered-By @hostname;',
+  match  => '^\s*location / {',
+}
+
+exec { 'restarting':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
