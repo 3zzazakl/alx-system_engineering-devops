bd2de0e5c1a369aec9e01373757b89338f08544e
3zzazakl <vanzekovichgr@gmail.com>
2024-05-02 01:37:56 +0300

    fixing fucking replication error in slave server

--- 0x14-mysql/setup.sql
+++ 0x14-mysql/setup.sql
@@ -5,8 +5,16 @@ USE tyrell_corp;
 CREATE TABLE nexus6 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));
 INSERT INTO nexus6 (name) VALUES ("AZZAZ");
 GRANT SELECT ON tyrell_corp.* TO 'holberton_user'@'localhost';
-CREATE USER 'replica_user'@'%' IDENTIFIED BY 'replica_user';
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+CREATE USER 'replica_user'@'18.210.17.11' IDENTIFIED BY 'replica_user';
+GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replica_user'@'18.210.17.11';
 GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
 CREATE USER web02@18.210.17.11 IDENTIFIED BY "web02";
-GRANT REPLICATION SLAVE ON *.* TO 'web02'@'18.210.17.11';
+GRANT REPLICATION SLAVE ON *.* TO 'web02'@18.210.17.11;
+
+#solving errors in slave server:
+CREATE USER 'replica_user'@'%' IDENTIFIED By 'replica_password';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+FLUSH PRIVILEGES;
+STOP SLAVE;
+START SLAVE;
+SHOW SLAVE STATUS\G;
