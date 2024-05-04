ba4f1b284101e46caa035d712d5619d3f9a263b9
3zzazakl <vanzekovichgr@gmail.com>
2024-05-05 01:23:16 +0300

    fixing task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,19 +9,17 @@ response_user = requests.get(
 reponse_todos = requests.get(
     f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
 
-if response_user.status_code != 200:
-    print("Error: Not a valid user id")
-    sys.exit(1)
 
-if reponse_todos.status_code != 200:
-    print("Error: Not a valid user id")
-    sys.exit(1)
+if __name__ == "__main__":
+    if response_user.status_code != 200 or reponse_todos.status_code != 200:
+        print("Error: Not a valid user id")
+        sys.exit(1)
 
-ud = response_user.json()
-todo = reponse_todos.json()
+    ud = response_user.json()
+    todo = reponse_todos.json()
 
-dtsk = [task for task in todo if task.get("completed") is True]
+    dtsk = [task for task in todo if task.get("completed")]
 
-print(f"Employee {ud.get('name')} is done with tasks({len(dtsk)}{len(todo)}):")
-for task in dtsk:
-    print(f"\t {task.get('title')}")
+    print(f"Employee {ud.get('name')} is done with tasks({len(dtsk)}{len(todo)}):")
+    for task in dtsk:
+        print(f"\t {task.get('title')}")
