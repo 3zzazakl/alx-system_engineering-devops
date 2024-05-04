9974a11d0a885f463c8966ef20ee7a22b9b2d099
3zzazakl <vanzekovichgr@gmail.com>
2024-05-05 01:56:22 +0300

    checking taks2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Export data in the CSV format """
+import csv
+import requests
+import sys
+
+
+def cvs_writer(employeeId):
+    """ Export data in the CSV format """
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            employeeId)).json().get("username")
+    tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            employeeId)).json()
+
+    with open("{}.csv".format(employeeId), "w") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+
+        for task in tasks:
+            writer.writerow({
+                "USER_ID": task.get("userId"),
+                "USERNAME": name,
+                "TASK_COMPLETED_STATUS": task.get("completed"),
+                "TASK_TITLE": task.get("title")
+            })
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        cvs_writer(sys.argv[1])
