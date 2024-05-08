8312e736deed12ab3721e11b6529fcfb60e9547c
3zzazakl <vanzekovichgr@gmail.com>
2024-05-09 00:50:11 +0300

    checking

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# fix server error 
+
+exec { 'replace':
+    provider => shell,
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+    only_if  => '/var/www/html/wp-settings.php',
+    notify   => 'Restart apache2'
+}
