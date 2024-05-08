a264ba03600ea3789d24bcfdfea1998fefe21e69
3zzazakl <vanzekovichgr@gmail.com>
2024-05-09 00:55:14 +0300

    chekcing

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,6 +3,6 @@
 exec { 'replace':
     provider => shell,
     command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-    only_if  => '/var/www/html/wp-settings.php',
+    only_if  => 'grep -q "php" /var/www/html/wp-settings.php',
     notify   => 'Restart apache2'
 }
