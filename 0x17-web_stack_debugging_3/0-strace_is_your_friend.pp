5adc333c4886f8bd6bdc3920e046d96075029979
3zzazakl <vanzekovichgr@gmail.com>
2024-05-09 00:57:12 +0300

    removing onlyif erorr

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,5 @@
 
 exec { 'replace':
     provider => shell,
-    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-    only_if  => 'grep -q "php" /var/www/html/wp-settings.php',
-    notify   => 'Restart apache2'
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
