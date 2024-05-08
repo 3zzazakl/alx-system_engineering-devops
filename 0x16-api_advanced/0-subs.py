7e62792dda564a50be2d05e325379c9a1604b347
3zzazakl <vanzekovichgr@gmail.com>
2024-05-08 04:38:45 +0300

    checking subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""how many subreddits"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Number of subscribers of a subreddit."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
