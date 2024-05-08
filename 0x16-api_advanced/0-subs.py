d29c5780f47dcb982e9f480557868a32bd0be53e
3zzazakl <vanzekovichgr@gmail.com>
2024-05-08 04:46:59 +0300

    checing errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,6 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
+        return response.json()['data']['subscribers']
     else:
         return 0
