9e1fde99cc2d82f7eb631acc1cf989f5662c6373
3zzazakl <vanzekovichgr@gmail.com>
2024-05-16 07:20:07 +0300

    checking 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
-# 
+# replace the default ulimit value in /etc/default/nginx
+
 exec { 'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
